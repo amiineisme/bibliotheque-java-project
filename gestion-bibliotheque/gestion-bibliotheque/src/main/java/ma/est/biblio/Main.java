8bac80e17058b74978c969602fe2b400608be3f5
amiineisme <amiineits@gmail.com>
2025-12-19 06:03:19 +0100

    Delete gestion-bibliotheque/gestion-bibliotheque directory

--- gestion-bibliotheque/gestion-bibliotheque/src/main/java/ma/est/biblio/Main.java
+++
@@ -1,9 +0,0 @@
-package ma.est.biblio;
-
-import ma.est.biblio.ui.LoginUI;
-
-public class Main {
-    public static void main(String[] args) {
-        new LoginUI();
-    }
-}
--- gestion-bibliotheque/gestion-bibliotheque/src/main/java/ma/est/biblio/dao/UtilisateurDAO.java
+++
@@ -1,8 +0,0 @@
-package ma.est.biblio.dao;
-
-import ma.est.biblio.model.Utilisateur;
-
-public interface UtilisateurDAO {
-    Utilisateur findByLogin(String login);
-    void save(Utilisateur utilisateur); 
-}
